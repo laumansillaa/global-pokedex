31eca282b8bd2bcbe2c93ce44fda64ba0066593e
laumansillaa <laumansillaa@hotmail.com>
2024-03-03 23:54:37 -0300

    updated logo-welcome - add viteCompression

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      algorithm: "brotliCompress",
+      disable: false,
+      ext: ".br",
+      filter: /\.(js|css|json|txt|html|ico|svg|md|ttf)$/,
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-})
+});
